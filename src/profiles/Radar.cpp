920c1992ea48aabe12da274a1274f78b08955343
Kemal Hadimli <disq@users.noreply.github.com>
2022-05-28 20:07:05 +0100

    Update radar, add example

--- src/profiles/Radar.cpp
+++ src/profiles/Radar.cpp
@@ -24,7 +24,7 @@ void RadarThreatsPage::Decode(uint8_t const* buffer) {
 
     threats[0].range_meters = (p_incoming_data->ranges[0] & 0xFC) >> 2;
     threats[1].range_meters = ((p_incoming_data->ranges[0] & 0x03) << 4) | ((p_incoming_data->ranges[1] & 0xF0) >> 4);
-    threats[2].range_meters = ((p_incoming_data->ranges[1] & 0x0F) << 4) | ((p_incoming_data->ranges[2] & 0xC0) >> 6);
+    threats[2].range_meters = ((p_incoming_data->ranges[1] & 0x0F) << 2) | ((p_incoming_data->ranges[2] & 0xC0) >> 6);
     threats[3].range_meters = (p_incoming_data->ranges[1] & 0x3F);
 
     threats[0].speed_mps = (p_incoming_data->speeds[0] & 0xF0) >> 4;
@@ -33,18 +33,71 @@ void RadarThreatsPage::Decode(uint8_t const* buffer) {
     threats[3].speed_mps = (p_incoming_data->speeds[1] & 0x0F);
 }
 
+//
+
+RadarStatusPage::RadarStatusPage() :
+   status(ANT_RADAR_STATUS_ABORTING_SHUTDOWN)
+{
+}
+
+void RadarStatusPage::Decode(uint8_t const* buffer) {
+    ant_radar_status_data_layout_t const * p_incoming_data = (ant_radar_status_data_layout_t *)buffer;
+
+    status = (ant_radar_status_t)(p_incoming_data->status & 0x03);
+}
+
+//
+
+RadarVendorInfoPage::RadarVendorInfoPage() :
+   info{}
+{
+}
+
+void RadarVendorInfoPage::Decode(uint8_t const* buffer) {
+    ant_radar_vendor_info_data_layout_t const * p_incoming_data = (ant_radar_vendor_info_data_layout_t *)buffer;
+
+    info.hw_revision = p_incoming_data->hw_revision;
+    info.manufacturer_id = (uint32_t)((p_incoming_data->manuf_id_HSB << 8) + p_incoming_data->manuf_id_LSB);
+    info.model_number = (uint32_t)((p_incoming_data->model_number_HSB << 8) + p_incoming_data->model_number_LSB);
+}
+
+//
+
+RadarDeviceInfoPage::RadarDeviceInfoPage() :
+   info{}
+{
+}
+
+void RadarDeviceInfoPage::Decode(uint8_t const* buffer) {
+    ant_radar_device_info_data_layout_t const * p_incoming_data = (ant_radar_device_info_data_layout_t *)buffer;
+
+    if (p_incoming_data->sw_version_supplemental == 0xFF) {
+      info.version = float(int(p_incoming_data->sw_version_main) / 10);
+    } else {
+      info.version = float((int(p_incoming_data->sw_version_main) * 100) + int(p_incoming_data->sw_version_supplemental)) / 1000;
+    }
+    info.serial = (uint32_t)((p_incoming_data->serial[3] << 24) + (p_incoming_data->serial[2] << 16) + (p_incoming_data->serial[1] << 8) + p_incoming_data->serial[0]);
+}
+
+//
+
 Radar::Radar(ANTTransmissionMode mode) : ANTProfile(mode) {
    m_disp_config.channel_type      = RADAR_DISP_CHANNEL_TYPE;
    m_disp_config.ext_assign        = RADAR_EXT_ASSIGN;
    m_disp_config.rf_freq           = RADAR_ANTPLUS_RF_FREQ;
    m_disp_config.transmission_type = 0;
    m_disp_config.device_type       = RADAR_DEVICE_TYPE;
-   m_disp_config.channel_period    = RADAR_MSG_PERIOD_4Hz;
+   m_disp_config.channel_period    = RADAR_MSG_PERIOD_8Hz;
    m_disp_config.network_number    = ANTPLUS_NETWORK_NUMBER;
    m_disp_config.device_number     = 0;
 
-  _OnThreatLevelsUpdated_cb = NULL;
+  vehicles_detected = false;
+
+  _OnVehicleStatusChanged_cb = NULL;
   _OnThreatDataUpdated_cb = NULL;
+  _OnStatusUpdated_cb = NULL;
+  _OnVendorInfoUpdated_cb = NULL;
+  _OnDeviceInfoUpdated_cb = NULL;
 }
 
 void Radar::EncodeMessage() {
@@ -54,7 +107,6 @@ void Radar::DecodeMessage(uint8_t* buffer) {
     ant_radar_message_layout_t* p_radar_message_payload = (ant_radar_message_layout_t *)buffer;
 
     int decoded_page_threat_index = -1;
-
     switch (p_radar_message_payload->page_id) {
         case ANT_RADAR_THREATS_A:
             threats[0].Decode(p_radar_message_payload->page_payload);
@@ -66,17 +118,46 @@ void Radar::DecodeMessage(uint8_t* buffer) {
             decoded_page_threat_index = 1;
             break;
 
+        case ANT_RADAR_STATUS:
+            statusPage.Decode(p_radar_message_payload->page_payload);
+            if (_OnStatusUpdated_cb != NULL) {
+              _OnStatusUpdated_cb(statusPage.GetStatus());
+            }
+            break;
+
+        case ANT_RADAR_VENDOR_INFO:
+            vendorInfoPage.Decode(p_radar_message_payload->page_payload);
+            if (_OnVendorInfoUpdated_cb != NULL) {
+              _OnVendorInfoUpdated_cb(vendorInfoPage.GetInfo());
+            }
+            break;
+
+        case ANT_RADAR_DEVICE_INFO:
+            deviceInfoPage.Decode(p_radar_message_payload->page_payload);
+            if (_OnDeviceInfoUpdated_cb != NULL) {
+              _OnDeviceInfoUpdated_cb(deviceInfoPage.GetInfo());
+            }
+            break;
+
         default:
             break;
     }
 
-    if (decoded_page_threat_index > -1 && _OnThreatLevelsUpdated_cb != NULL) {
-        _OnThreatLevelsUpdated_cb(decoded_page_threat_index, threats[decoded_page_threat_index].GetLevels());
-    }
-    if (decoded_page_threat_index > -1 && _OnThreatDataUpdated_cb != NULL) {
+    if (decoded_page_threat_index > -1) {
+      if (_OnVehicleStatusChanged_cb != NULL) {
+        bool gotVehicles = threats[decoded_page_threat_index].GetLevels() > 0;
+        if (gotVehicles != vehicles_detected) {
+          vehicles_detected = gotVehicles;
+          _OnVehicleStatusChanged_cb(vehicles_detected);
+        }
+      }
+      
+      if (_OnThreatDataUpdated_cb != NULL) {
         ant_radar_threats_t thr[RADAR_NUM_THREATS_PER_PAGE];
         for(int i = 0; i < RADAR_NUM_THREATS_PER_PAGE; i ++) thr[i] = threats[decoded_page_threat_index].GetThreats(i);
         _OnThreatDataUpdated_cb(decoded_page_threat_index, thr);
+      }
+
     }
 
 }
--- src/profiles/Radar.h
+++ src/profiles/Radar.h
@@ -3,18 +3,15 @@
 
 #include <stdint.h>
 
-#include "ANTProfile.h"
+#include "../ANTProfile.h"
 
 
-#define RADAR_DEVICE_TYPE     0x28u     ///< Device type reserved for ANT+ radar.
-#define RADAR_ANTPLUS_RF_FREQ   0x39u     ///< Frequency, decimal 57 (2457 MHz).
+#define RADAR_DEVICE_TYPE       40     // Device type reserved for ANT+ radar
+#define RADAR_ANTPLUS_RF_FREQ   57     // Frequency, 2457 MHz
+#define RADAR_MSG_PERIOD_8Hz    0xFF4u // Message period, ~8hz
 
-#define RADAR_MSG_PERIOD_4Hz    0x1F86u   ///< Message period, decimal 8070 (4.06 Hz).
-#define RADAR_MSG_PERIOD_2Hz    0x3F0Cu   ///< Message period, decimal 16140 (2.03 Hz).
-#define RADAR_MSG_PERIOD_1Hz    0x7E18u   ///< Message period, decimal 32280 (1.02 Hz).
-
-#define RADAR_EXT_ASSIGN        0x00                  ///< ANT ext assign. FIXME ?
-#define RADAR_DISP_CHANNEL_TYPE CHANNEL_TYPE_SLAVE    ///< Display RADAR channel type.
+#define RADAR_EXT_ASSIGN        0x00                          // ANT ext assign
+#define RADAR_DISP_CHANNEL_TYPE CHANNEL_TYPE_SLAVE_RX_ONLY    // Display RADAR channel type.
 
 #define RADAR_NUM_THREATS_PER_PAGE 4
 
@@ -64,14 +61,89 @@ private:
 
 };
 
+typedef enum {
+  ANT_RADAR_STATUS_ABORTING_SHUTDOWN = 0,
+  ANT_RADAR_STATUS_SHUTDOWN = 1
+} ant_radar_status_t;
+
+class RadarStatusPage {
+public:
+   RadarStatusPage();
+   ant_radar_status_t GetStatus() { return status; };
+   void Decode(uint8_t const* buffer);
+
+private:
+    ant_radar_status_t status;
+
+   typedef struct
+   {
+      uint8_t status;
+      uint8_t reserved[6];
+   }ant_radar_status_data_layout_t;
+};
+
+typedef struct
+{
+  uint8_t hw_revision;
+  uint16_t manufacturer_id;
+  uint16_t model_number;
+}ant_radar_vendor_info_t;
+
+class RadarVendorInfoPage {
+public:
+   RadarVendorInfoPage();
+   ant_radar_vendor_info_t GetInfo() { return info; };
+   void Decode(uint8_t const* buffer);
+
+private:
+    ant_radar_vendor_info_t info;
+
+   typedef struct
+   {
+      uint8_t reserved[2];
+      uint8_t hw_revision;
+      uint8_t manuf_id_LSB;
+      uint8_t manuf_id_HSB;
+      uint8_t model_number_LSB;
+      uint8_t model_number_HSB;
+   }ant_radar_vendor_info_data_layout_t;
+};
+
+typedef struct
+{
+  float version;
+  uint32_t serial;
+}ant_radar_device_info_t;
+
+class RadarDeviceInfoPage {
+public:
+   RadarDeviceInfoPage();
+   ant_radar_device_info_t GetInfo() { return info; };
+   void Decode(uint8_t const* buffer);
+
+private:
+    ant_radar_device_info_t info;
+
+   typedef struct
+   {
+      uint8_t reserved;
+      uint8_t sw_version_supplemental;
+      uint8_t sw_version_main;
+      uint8_t serial[4];
+   }ant_radar_device_info_data_layout_t;
+};
+
 class Radar : public ANTProfile
 {
 public:
    Radar(ANTTransmissionMode mode);
 
    void ProcessMessage(ant_evt_t*);
-   void SetOnThreatLevelsUpdated(void (*fp)(int, int)) { _OnThreatLevelsUpdated_cb = fp;}
-   void SetOnThreatDataUpdated(void (*fp)(int, ant_radar_threats_t[4])) { _OnThreatDataUpdated_cb = fp;}
+   void SetOnVehicleStatusChanged(void (*fp)(bool)) { _OnVehicleStatusChanged_cb = fp; }
+   void SetOnThreatDataUpdated(void (*fp)(int, ant_radar_threats_t[4])) { _OnThreatDataUpdated_cb = fp; }
+   void SetOnStatusUpdated(void (*fp)(ant_radar_status_t)) { _OnStatusUpdated_cb = fp; }
+   void SetOnVendorInfoUpdated(void (*fp)(ant_radar_vendor_info_t)) { _OnVendorInfoUpdated_cb = fp; }
+   void SetOnDeviceInfoUpdated(void (*fp)(ant_radar_device_info_t)) { _OnDeviceInfoUpdated_cb = fp; }
 
 private:
 
@@ -79,7 +151,11 @@ private:
    typedef enum
    {
       ANT_RADAR_THREATS_A = 48,
-      ANT_RADAR_THREATS_B = 49
+      ANT_RADAR_THREATS_B = 49,
+      ANT_RADAR_STATUS = 1,
+      ANT_RADAR_VENDOR_INFO = 80,
+      ANT_RADAR_DEVICE_INFO = 81
+//      ANT_RADAR_BATT_INFO = 82
    } ant_radar_page_t;
 
    /**@brief Radar message data layout structure. */
@@ -90,12 +166,22 @@ private:
    } ant_radar_message_layout_t;
 
    RadarThreatsPage threats[2];
+   RadarStatusPage statusPage;
+   RadarVendorInfoPage vendorInfoPage;
+   RadarDeviceInfoPage deviceInfoPage;
 
+   bool vehicles_detected;
+
+   void (*_OnVehicleStatusChanged_cb) (bool);
    void (*_OnThreatLevelsUpdated_cb) (int, int);
    void (*_OnThreatDataUpdated_cb) (int, ant_radar_threats_t[RADAR_NUM_THREATS_PER_PAGE]);
+   void (*_OnStatusUpdated_cb) (ant_radar_status_t);
+   void (*_OnVendorInfoUpdated_cb) (ant_radar_vendor_info_t);
+   void (*_OnDeviceInfoUpdated_cb) (ant_radar_device_info_t);
 
    void EncodeMessage();
    void DecodeMessage(uint8_t* p_message_payload);
 };
+
 
 #endif
